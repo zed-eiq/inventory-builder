72d823318fc378f61885f58a36340d9a1035cde0
zed tan <wtan@eclecticiq.com>
2023-03-06 04:18:39 +0100

    add build script

---
+++ make-dist.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+git submodule update --init --remote
+
+pushd docs-extensions
+python3 -m venv .venv && source .venv/bin/activate
+python -m pip install -r requirements.in
+sphinx-build -E -b dirhtml source build/dirhtml
+cp build/dirhtml/objects.inv ../dist/docs-extensions_objects.inv
+deactivate
+popd
+
+pushd docs-intel-center
+python3 -m venv .venv && source .venv/bin/activate
+python -m pip install -r requirements.in
+git submodule update --init --remote
+sphinx-build -E -b dirhtml source build/dirhtml
+cp build/dirhtml/objects.inv ../dist/docs-intel-center_objects.inv
+deactivate
+popd
